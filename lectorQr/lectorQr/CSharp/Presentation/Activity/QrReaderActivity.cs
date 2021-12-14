91044f089fa3c493da9dcb45ce388c44589176a3
Pablo <pab.garridom@gmail.com>
2021-12-14 01:20:14 -0300

    add scan qr

---
+++ lectorQr/lectorQr/CSharp/Presentation/Activity/QrReaderActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using ZXing.Mobile;
+
+namespace lectorQr.CSharp.Presentation.Activity
+{
+    [Activity(Label = "Lector Qr", Theme = "@style/AppTheme")]
+    public class QrReaderActivity : AppCompatActivity
+    {
+        Button enterButton;
+        TextView codeText;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_qr_reader);
+            codeText = FindViewById<TextView>(Resource.Id.codeText);
+            enterButton = FindViewById<Button>(Resource.Id.enterButton);
+            enterButton.Click += btnQrReaderClick;
+        }
+
+        private async void btnQrReaderClick(object sender, EventArgs e)
+        {
+            MobileBarcodeScanner.Initialize(Application);
+            var scanner = new ZXing.Mobile.MobileBarcodeScanner();
+
+            var result = await scanner.Scan();
+
+            if (result != null)
+                codeText.Text = result.Text;
+        }
+    }
+}
