3aa6e34f15bcd8ecbefc7b8ccdccc98fa8a3bb2d
Pablo <pab.garridom@gmail.com>
2021-12-14 00:51:26 -0300

    splash incorporation of the app

---
+++ lectorQr/lectorQr/CSharp/Presentation/Activity/SplashActivity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using lectorQr.CSharp.Presentation.Activity;
+using lectorQr.CSharp.Presentation.Contract;
+
+namespace lectorQr
+{
+    [Activity(Theme = "@style/Splash", MainLauncher = true)]
+    public class SplashActivity : AppCompatActivity, SplashContract
+    {      
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_splash);
+            this.initView();
+        }       
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public int initView()
+        {
+            Task startupWork = new Task(() => {
+                Task.Delay(3000);
+            });
+
+            startupWork.ContinueWith(async t => {
+                StartActivity(new Intent(this, typeof(QrReaderActivity)));
+                Finish();
+            });
+
+            startupWork.Start();
+
+            return 0;
+        }    
+    }
+}
